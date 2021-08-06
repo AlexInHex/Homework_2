2dcb8492531c0913af3c441532e1b27decff5721
Alexey Vlasenko <alexinhex@gmail.com>
2021-08-06 16:46:15 +0300

    Создал ввод матрицы с клавиатуры, возможность выбора размера матрицы, вывод введенной матрицы в консоль, и вывод на экран количества положительных и отрицательных членов матрицы.

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -5,8 +5,71 @@ namespace Homework_2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+           
+            Console.WriteLine("Введите число строк матрицы: ");
+            var rows = Console.ReadLine();
+
+            Console.WriteLine("Введите число столбцов матрицы: ");
+            var cols = Console.ReadLine();
+            
+            int x = int.Parse(rows);
+            int y = int.Parse(cols);
+
+            int[,] matrix = new int[y, x];
+
+            int positiveNum = 0;
+            int negativeNum = 0;
+
+            // Ввод матрицы с клавиатуры.
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine("Y " + i + " X " + j);
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            //Вывод матрицы в консоль таблицей.
+            Console.WriteLine();
+            Console.WriteLine("Введенная матрица↓↓↓");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("  " + matrix[i,j]);
+                }
+                Console.WriteLine("  ");
+            }
+
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("1 - Найти количество положительных и отрицательных чисел в матрице");
+            Console.WriteLine("2 - сортировать элементы в строках матрицы (по убыванию)");
+            Console.WriteLine("3 - инвертировать все строки в матрице");
+            Console.WriteLine("----------------------------------------------------------------------");
+
+            //Поиск количества положительных и отрицательных членов.
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] > 0)
+                    {
+                        positiveNum++;
+                    }
+
+                    if (matrix[i, j] < 0)
+                    {
+                        negativeNum++;
+                    }
+                }
+            }
+            Console.WriteLine("Количество положительных элементов = " + positiveNum);
+            Console.WriteLine("Количество отрицательных элементов = " + negativeNum);
+
+
+
         }
     }
 }
